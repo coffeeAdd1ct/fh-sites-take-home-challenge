7547bb6c09806ce1a8d5acd2b9bf96adf7df9339
Kevin <billeaudk@gmail.com>
2020-03-22 18:16:33 -0600

    3 passing

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -1,9 +1,17 @@
+var Solver = require("pokersolver").Hand; //https://github.com/goldfire/pokersolver
+
 class PokerHand {
-  constructor() {}
+  constructor(hand_dealt) {
+    //read cards dealt
+    this.hand_dealt = hand_dealt;
+  }
 
   getRank() {
-    // Implement poker hand ranking
-    return 'Royal Flush';
+    //put cards in array, solve the cards dealt
+    var solved_hand = Solver.solve(this.hand_dealt.split(" "));
+
+    //return type of hand
+    return solved_hand.name;
   }
 }
 
